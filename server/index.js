bed8455e483e036d6f6fa47cb19129aaa33ec2a7
Hurley <86563399+Hurleywflow@users.noreply.github.com>
2022-07-13 20:21:39 +1000

    mit
    preparing for deploy

--- server/index.js
+++ server/index.js
@@ -30,12 +30,12 @@ if (process.env.NODE_ENV === 'production') {
   // server path join to the frontend
   app.use(express.static(path.join(__dirname, '/client/build')));
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     // get index.html from the frontend to show
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.send('Server is running');
   });
 }
