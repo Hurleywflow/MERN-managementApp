fa70676b7b26b0c401e3cbbcb3b35e3b2137d146
Hurley <86563399+Hurleywflow@users.noreply.github.com>
2022-08-24 10:38:12 +1000

    redeploy

--- server/index.js
+++ server/index.js
@@ -50,6 +50,19 @@ app.get('*', (req, res) => {
 //   });
 // }
 
+// or
+// app.use(express.static(path.join(__dirname, './frontend/build')));
+
+// app.get('*', function (_, res) {
+// 	res.sendFile(
+// 		path.join(__dirname, './frontend/build/index.html'),
+// 		function (err) {
+// 			if (err) {
+// 				res.status(500).send(err);
+// 			}
+// 		}
+// 	);
+// });
 
 
 const port = process.env.PORT || 4000;
